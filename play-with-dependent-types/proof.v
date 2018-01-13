bc80621105ff9432c25639e53622ec50c536dfcc
Siddharth Bhat <siddu.druid@gmail.com>
2018-01-13 16:43:58 +0530

    complete proofs with dependent types based on proof

--- play-with-dependent-types/proof.v
+++ play-with-dependent-types/proof.v
@@ -1,42 +1,18 @@
-Require Import Coq.Lists.List.
-Require Import FunInd.
-Require Import Coq.Lists.ListSet.
-Require Import Coq.ZArith.BinInt.
-Require Import Coq.ZArith.BinIntDef.
-Require Import Coq.Numbers.BinNums.
-Require Import Coq.Lists.List.
-Require Import Coq.ZArith.Zbool.
-Require Import Coq.Sorting.Permutation.
-Require Import Coq.Logic.FunctionalExtensionality.
-Require Import Omega.
-Require Import Coq.Bool.Sumbool.
-Require Import Coq.ZArith.Zhints.
-Require Import Coq.Classes.EquivDec.
-Require Import Coq.Structures.Equalities.
-Require Import Coq.FSets.FMapInterface.
-Require Import FMapAVL.
-Require Import FMapFacts.
-Require Import Coq.Logic.FinFun.
-Require Import Nat.
-
-(* thank you kind stranger for showing me functorial modules syntax *)
-(* http://newartisans.com/2016/10/using-fmap-in-coq/ *)
-
-Require Import
-  Coq.FSets.FMapList
-  Coq.Structures.OrderedTypeEx.
-
-Module Import M := FMapList.Make(Z_as_OT).
-Module Import MFacts := WFacts(M).
-
-
-Require Import
-        Coq.FSets.FMapFacts.
-
+Import EqNotations.
 Inductive dep (n: nat) :=
   mkDep : dep n.
 
 
-Theorem equalInhabitants (n n': nat): n = n' -> dep n = dep n'.
+Theorem equalTypes (n n': nat): n = n' -> dep n = dep n'.
 Proof.
   intros.
+  destruct H.
+  reflexivity.
+Qed.
+
+
+Theorem equalInhabitants (n n' : nat): forall (H: n = n'),
+    (rew H in (mkDep n)) = mkDep n'.
+  intros.
+  subst. reflexivity.
+Qed.
